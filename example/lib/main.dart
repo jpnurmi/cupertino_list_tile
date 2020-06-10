ca11123e249e085b984026451649b29a67183ae7
J-P Nurmi <jpnurmi@gmail.com>
2020-06-10 18:51:44 +0200

    Example: add dark mode & selected/disabled list tiles

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -3,23 +3,43 @@ import 'package:cupertino_list_tile/cupertino_list_tile.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool dark = false;
+  int selectedIndex = -1;
+
   @override
   Widget build(BuildContext context) {
     return CupertinoApp(
       title: 'CupertinoListTile',
+      theme: CupertinoThemeData(
+        brightness: dark ? Brightness.dark : Brightness.light,
+      ),
       home: CupertinoPageScaffold(
         navigationBar: CupertinoNavigationBar(
           middle: Text('CupertinoListTile'),
+          trailing: CupertinoSwitch(
+            value: dark,
+            onChanged: (value) => setState(() => dark = value),
+          ),
         ),
         child: CupertinoScrollbar(
           child: ListView.builder(
             itemCount: 100,
             itemBuilder: (BuildContext context, int index) {
+              final selected = index == selectedIndex;
+              final enabled = index == 0 || index % 10 != 0;
               return ListTile(
-                title: Text('#$index'),
-                subtitle: Text('CupertinoListTile'),
-                onTap: () => print('tap: #$index'),
+                title: Text('CupertinoListTile #$index'),
+                enabled: enabled,
+                selected: selected,
+                subtitle: Text(
+                    'A ${selected ? 'selected' : !enabled ? 'disabled' : 'normal'} CupertinoListTile'),
+                onTap: () => setState(() => selectedIndex = index),
               );
             },
           ),
